1121e0fb11d4d505a275624b264b2b2600cc7667
RG Raja Sekar <rgraj1998@gmail.com>
2016-11-30 15:16:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ var articles = {
     }
 };
 
-function creatTemplate (data) {
+function createTemplate (data) {
     var title = data.title;
     var date   = data.date;
     var heading = data.heading;
