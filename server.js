b7fedd05159f473e9d76e9c033caaccca275de33
RG Raja Sekar <rgraj1998@gmail.com>
2016-11-30 14:36:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
+var articles = {
+   'article-one': {
     title:  'Article one I RG RAj',
     heading: 'Article one',
     date: 'Sep 5,2016',
@@ -20,21 +21,73 @@ var articleOne = {
         <p>
              this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the cintent for my first article.
         </p>`
+},
+   'article-two': {
+      title:  'Article Two I RG Raj',
+      heading: 'Article Two',
+      date: 'Sep 10,2016',
+      content: `
+         <p>
+               This is the content for my second article.
+         </p>`
+    },
+   'article-three': {
+      title: 'Article Three I RG Raj',
+      heading:  'Article Three',
+      date: 'Sep 15,2016',
+      content: `
+         <p>
+               This is the content for my third article.
+         </p>`
+    }
 };
+
+function creatTemplate (data) {
+    var title = data.title;
+    var date   = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewreport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head>
+      <body>
+          <div class="container">
+             </div>
+                  <a href="/">Home</a>
+             </div>
+             <hr/>
+             <h3>
+                 ${heading}
+             </h3>
+             <div>
+                ${date}
+             </div>
+             <div>
+               ${content}
+             </div>
+          </div>
+      </body>
+    <html>
+    `;
+    return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname,  'ui',  'article-one.html'));
-});
-app.get('/article-two', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui',  'article-two.html'));
-});
-
-app.get('/article-three', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui',  'article-three.html'));
+app.get('/:articlename', function (req, res) {
+    // articleName == article-one
+    // articles(articleName) == {} content object for article one
+    var articleName = req.params.articleName;
+    res.send(createtemplate(articleone));
 });
 
 app.get('/ui/style.css', function (req, res) {
