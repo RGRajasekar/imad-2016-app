66a0718d108794bc1cbf02b2217c360ec190ee14
RG Raja Sekar <rgraj1998@gmail.com>
2016-11-30 17:14:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML=  'New Value';
 var img = document.getElementById('madi');
 var marginleft =0;
 function moveright () {
-    marginleft = marginleft +10;
+    marginleft = marginleft +1;
     img.style.marginleft = marginleft +'px';
 }
 img.onclick = function () {
